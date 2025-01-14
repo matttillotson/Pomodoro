0bd4fa7b1ce3076216f0543f6ef7dfbcb7f3c991
Matt Tillotson <matttillotson@icloud.com>
2025-01-14 15:47:06 -0500

    Remove all files

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Books App initialized');
-    
-    const addBookButton = document.getElementById('add-book');
-    addBookButton.addEventListener('click', () => {
-        console.log('Add book clicked');
-        // Add book functionality will go here
-    });
-}); 
